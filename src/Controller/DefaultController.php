c8437a81622681acf6ed8f9a047b7910fcd6d9dc
krustea <thibault.tregret@gmail.com>
2018-11-26 17:03:53 +0100

    integration html css

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,20 +1,20 @@
 <?php
 
-
 namespace App\Controller;
 
 
-class DefaultController extends BaseController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
 {
 
     /**
-     *@Route("/" , name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function homepage()
+    public function index()
     {
-        $books= $this->getDoctrine()->getRepository(Book::class)->findLast(6);
-        $categories = $this->getDoctrine()->getRepository(Category::class)->findBy([],['name'=>'ASC']);
-        return $this->render('default/homepage.html.twig',["books"=> $books, "categories"=>$categories]);
+        return $this->render('default/homepage.html.twig');
     }
 
 }
